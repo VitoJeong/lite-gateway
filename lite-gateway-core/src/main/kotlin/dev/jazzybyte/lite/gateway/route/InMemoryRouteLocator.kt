f4605e9805b616da64fc38a6f599f86775a366c7
changha <chjeong@aift.kr>
2025-06-20 10:19:37 +0900

    Change locate method to return Mono<Route> instead of Flux<Route>

--- lite-gateway-core/src/main/kotlin/dev/jazzybyte/lite/gateway/route/InMemoryRouteLocator.kt
+++ lite-gateway-core/src/main/kotlin/dev/jazzybyte/lite/gateway/route/InMemoryRouteLocator.kt
@@ -2,16 +2,15 @@ package dev.jazzybyte.lite.gateway.route
 
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 class InMemoryRouteLocator(
     private val routes: List<Route>
 ) : RouteLocator {
 
-    override fun locate(exchange: ServerWebExchange): Flux<Route> {
-        return Flux.fromIterable(
-            routes.filter { route ->
-                route.predicates.all { p -> p.matches(exchange) }
-            }
-        )
+    override fun locate(exchange: ServerWebExchange): Mono<Route> {
+    return routes.filter { route ->
+        route.predicates.all { p -> p.matches(exchange) }
+    }.firstNotNullOf { Mono.empty() }
     }
 }
--- lite-gateway-core/src/main/kotlin/dev/jazzybyte/lite/gateway/route/RouteLocator.kt
+++ lite-gateway-core/src/main/kotlin/dev/jazzybyte/lite/gateway/route/RouteLocator.kt
@@ -2,6 +2,7 @@ package dev.jazzybyte.lite.gateway.route
 
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 /**
  * 요청에 대한 라우팅 정보를 제공하는 인터페이스
@@ -13,5 +14,5 @@ interface RouteLocator {
      * 요청에 맞는 라우팅 정보들을 찾아 반환한다.
      * Flux 로 반환하는 이유 => reactive stream 기반의 동적 라우트 탐색이 가능하도록 하기 위함
      */
-    fun locate(exchange: ServerWebExchange): Flux<Route>
+    fun locate(exchange: ServerWebExchange): Mono<Route>
 }
