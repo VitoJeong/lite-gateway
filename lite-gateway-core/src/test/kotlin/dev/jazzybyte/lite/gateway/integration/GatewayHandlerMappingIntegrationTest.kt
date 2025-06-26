132174b05a01ff1cb68e636534d6a43a27e7ee8c
changha <chjeong@aift.kr>
2025-06-27 01:57:07 +0900

    GatewayHandlerMapping 통합테스트 테스트케이스 추가

--- lite-gateway-core/src/test/kotlin/dev/jazzybyte/lite/gateway/integration/GatewayHandlerMappingIntegrationTest.kt
+++ lite-gateway-core/src/test/kotlin/dev/jazzybyte/lite/gateway/integration/GatewayHandlerMappingIntegrationTest.kt
@@ -3,10 +3,15 @@ package dev.jazzybyte.lite.gateway.integration
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.configureFor
+import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.matching
+import com.github.tomakehurst.wiremock.client.WireMock.post
+import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.verify
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import dev.jazzybyte.lite.gateway.handler.FilterHandler
@@ -36,7 +41,6 @@ import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
-import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.DefaultUriBuilderFactory
 
 @SpringBootTest(
@@ -56,10 +60,14 @@ class GatewayHandlerMappingIntegrationTest {
     @LocalServerPort
     val port: Int = 0
 
+    /**
+     * 웹 엔드포인트를 테스트용 클라이언트
+     */
     lateinit var testClient: WebTestClient
 
-    lateinit var webClient: WebClient
-
+    /**
+     * 테스트용 기본 URI (scheme://host:port)
+     */
     lateinit var baseUri: String
 
 
@@ -77,16 +85,15 @@ class GatewayHandlerMappingIntegrationTest {
 
     protected fun setup(httpConnector: ClientHttpConnector?, baseUri: String) {
         this.baseUri = baseUri
-        this.webClient = WebClient.builder().clientConnector(httpConnector!!).baseUrl(this.baseUri).build()
         val uriBuilderFactory = DefaultUriBuilderFactory(this.baseUri)
-        this.testClient = WebTestClient.bindToServer(httpConnector)
+        this.testClient = WebTestClient.bindToServer(httpConnector!!)
             .uriBuilderFactory(uriBuilderFactory)
             .baseUrl(this.baseUri)
             .build()
     }
 
     @Test
-    fun `should route to correct handler`() {
+    fun `should route GET to mock server`() {
         // GIVEN
         // Stub 설정
         stubFor(
@@ -110,6 +117,60 @@ class GatewayHandlerMappingIntegrationTest {
         verify(getRequestedFor(urlEqualTo("/api/users")))
     }
 
+    @Test
+    fun `should route POST to mock server`() {
+        // GIVEN
+        stubFor(
+            post(urlEqualTo("/api/users"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withBody("POST users Match")
+                )
+        )
+
+        // WHEN
+        val responseSpec = testClient.post().uri("/api/users")
+            .exchange()
+
+        // THEN
+        responseSpec
+            .expectStatus().isOk
+            .expectBody<String>()
+            .isEqualTo("POST users Match")
+        verify(postRequestedFor(urlEqualTo("/api/users")))
+    }
+
+    @Test
+    fun `should route GET with query param to mock server`() {
+        // GIVEN
+        stubFor(
+            get(urlPathEqualTo("/api/param"))
+                .withQueryParam("param", matching(".*"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withBody("Query param: mock-value")
+                )
+        )
+
+        // WHEN
+        val responseSpec = testClient.get().uri { uriBuilder ->
+            uriBuilder.path("/api/param").queryParam("param", "mock-value").build()
+        }.exchange()
+
+        // THEN
+        responseSpec
+            .expectStatus().isOk
+            .expectBody<String>()
+            .isEqualTo("Query param: mock-value")
+        verify(
+            getRequestedFor(urlPathEqualTo("/api/param"))
+                .withQueryParam("param", equalTo("mock-value"))
+        )
+    }
+
+
     @EnableAutoConfiguration
     @SpringBootConfiguration
     class TestConfig {
@@ -125,7 +186,7 @@ class GatewayHandlerMappingIntegrationTest {
                         id = "test-route",
                         order = 0,
                         uri = "http://localhost:$mockPort",
-                        predicates = listOf(PathPredicate("/api/users"))
+                        predicates = listOf(PathPredicate("/api/*"))
                     )
                 )
             )
