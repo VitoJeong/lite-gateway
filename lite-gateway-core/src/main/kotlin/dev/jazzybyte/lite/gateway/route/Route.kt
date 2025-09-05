96f1eb6e055c7fd522cb8ef16c8690c342fa0598
changha <chjeong@aift.kr>
2025-09-06 02:35:52 +0900

    refactor(core): Route 모델의 모듈 의존성 제거
    core 모듈의 Route 클래스에서 webflux 의존성을 제거하고, predicate 필드를 Any 타입으로 변경하여 모듈 독립성을 확보합니다. 관련 설정 DTO인 RouteDefinition과 FilterDefinition을 autoconfigure 모듈에서 core 모듈로 이동하여 핵심 모델의 응집도를 높입니다.

--- lite-gateway-core/src/main/kotlin/dev/jazzybyte/lite/gateway/route/Route.kt
+++ lite-gateway-core/src/main/kotlin/dev/jazzybyte/lite/gateway/route/Route.kt
@@ -1,42 +1,20 @@
 package dev.jazzybyte.lite.gateway.route
 
 import dev.jazzybyte.lite.gateway.filter.core.GatewayFilter
-import dev.jazzybyte.lite.gateway.predicate.RoutePredicate
 import java.net.URI
 
 /**
  * 정의된 라우팅 정보
  */
 class Route(
-    private val _id: String,
-    private val _predicates: List<RoutePredicate>,
-    private var _uri: URI,
-    // 라우트의 우선순위, 정수 값이 낮을수록 먼저 처리됨
-    private val _order: Int,
-    private val _filters: List<GatewayFilter>,
+    val id: String,
+    val predicate: Any,
+    var uri: URI,
+    val order: Int,
+    val filters: List<GatewayFilter>,
 ) {
 
-    // 라우트의 고유 식별자
-    val id: String
-        get() = _id
-
-    // 라우트에 정의된 조건 목록
-    val predicates: List<RoutePredicate>
-        get() = _predicates
-
-    // 라우트가 가리키는 URI
-    var uri: URI
-        get() = _uri
-        private set(value) {
-            _uri = value
-        }
-
-    // 라우트의 우선순위
-    val order: Int
-        get() = _order
-
-    val filters: List<GatewayFilter>
-        get() = _filters
+    
 
     init {
         // URI가 null이거나 비어있는 경우 예외 발생
@@ -69,16 +47,10 @@ class Route(
     constructor(
         id: String,
         uri: String,
-        predicate: RoutePredicate,
+        predicate: Any,
         order: Int = Int.MAX_VALUE,
-    ) : this(id, listOf(predicate), createUriWithIdnSupport(uri), order, emptyList())
-
-    constructor(
-        id: String,
-        uri: String,
-        predicates: List<RoutePredicate>,
-        order: Int = Int.MAX_VALUE,
-    ) : this(id, predicates, createUriWithIdnSupport(uri), order, emptyList())
+        filters: List<GatewayFilter> = emptyList()
+    ) : this(id, predicate, createUriWithIdnSupport(uri), order, filters)
 
     companion object {
         /**
@@ -132,21 +104,21 @@ class Route(
 
         other as Route
 
-        if (_order != other._order) return false
-        if (_id != other._id) return false
-        if (_predicates != other._predicates) return false
-        if (_uri != other._uri) return false
-        if (_filters != other._filters) return false
+        if (id != other.id) return false
+        if (predicate != other.predicate) return false
+        if (uri != other.uri) return false
+        if (order != other.order) return false
+        if (filters != other.filters) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = _order
-        result = 31 * result + _id.hashCode()
-        result = 31 * result + _predicates.hashCode()
-        result = 31 * result + _uri.hashCode()
-        result = 31 * result + _filters.hashCode()
+        var result = id.hashCode()
+        result = 31 * result + predicate.hashCode()
+        result = 31 * result + uri.hashCode()
+        result = 31 * result + order
+        result = 31 * result + filters.hashCode()
         return result
     }
 
