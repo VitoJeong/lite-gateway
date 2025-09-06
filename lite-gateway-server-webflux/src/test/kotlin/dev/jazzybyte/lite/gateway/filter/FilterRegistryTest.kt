afd84a8fb990da6d2d90dd988518f088a92592f9
changha <chjeong@aift.kr>
2025-09-06 13:13:32 +0900

    test: FilterRegistry.getFilterClass null 반환케이스를 예외 처리로 업데이트

--- lite-gateway-server-webflux/src/test/kotlin/dev/jazzybyte/lite/gateway/filter/FilterRegistryTest.kt
+++ lite-gateway-server-webflux/src/test/kotlin/dev/jazzybyte/lite/gateway/filter/FilterRegistryTest.kt
@@ -1,8 +1,10 @@
 package dev.jazzybyte.lite.gateway.filter
 
 import dev.jazzybyte.lite.gateway.config.FilterRegistry
+import dev.jazzybyte.lite.gateway.exception.FilterDiscoveryException
 import dev.jazzybyte.lite.gateway.filter.core.GatewayFilter
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -13,7 +15,7 @@ class FilterRegistryTest {
     @Test
     @DisplayName("Filter 클래스들이 성공적으로 발견되어야 함")
     fun `should discover filter classes successfully`() {
-        
+
         // when
         val availableFilters = FilterRegistry.getAvailableFilterNames()
 
@@ -25,7 +27,7 @@ class FilterRegistryTest {
     @Test
     @DisplayName("등록된 Filter 이름으로 클래스를 조회할 수 있어야 함")
     fun `should be able to get filter class by name`() {
-        
+
         // given
         val availableFilters = FilterRegistry.getAvailableFilterNames()
         val firstFilterName = availableFilters.first()
@@ -39,20 +41,19 @@ class FilterRegistryTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 Filter 이름으로 조회하면 null을 반환해야 함")
-    fun `should return null for non-existent filter name`() {
-        
-        // when
-        val filterClass = FilterRegistry.getFilterClass("NonExistentFilter")
+    @DisplayName("존재하지 않는 Filter 이름으로 조회하면 예외가 발생해야 함")
+    fun `should throw exception for non-existent filter name`() {
 
-        // then
-        assertThat(filterClass).isNull()
+        // when & then
+        assertThatThrownBy { FilterRegistry.getFilterClass("NonExistentFilter") }
+            .isInstanceOf(FilterDiscoveryException::class.java)
+            .hasMessageContaining("is not registered")
     }
 
     @Test
     @DisplayName("Filter 등록 여부를 확인할 수 있어야 함")
     fun `should be able to check if filter is registered`() {
-        
+
         // given
         val availableFilters = FilterRegistry.getAvailableFilterNames()
         val firstFilterName = availableFilters.first()
