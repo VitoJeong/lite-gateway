829fe8384720ae5aed357b905c2547799779df50
changha <chjeong@aift.kr>
2025-09-11 23:49:10 +0900

    feat: HttpClientProperties 기본설정값 변경

--- lite-gateway-spring-boot-autoconfigure/src/main/kotlin/dev/jazzybyte/lite/gateway/client/HttpClientProperties.kt
+++ lite-gateway-spring-boot-autoconfigure/src/main/kotlin/dev/jazzybyte/lite/gateway/client/HttpClientProperties.kt
@@ -5,9 +5,9 @@ import org.springframework.validation.annotation.Validated
 @Validated
 class HttpClientProperties (
     val maxConnections: Int = 500,
-    val connectionTimeout: Int = 30 * 1000,
-    val maxHeaderSize: Int = 30 * 1000,
-    val acquireTimeout: Int = 20 * 1000,
+    val connectionTimeout: Int = 5 * 1000,
+    val maxHeaderSize: Int = 8192,
+    val acquireTimeout: Long = 10 * 1000
 ) {
 
 }
